8eddb5b5829206023550109ce851b5028c953a55
Evan Zuo <739221432@qq.com>
2023-10-05 10:23:31 +0800

    feat: dept

---
+++ business-core/business-menu/src/main/java/cn/evanzuo/admin/business/menu/controller/SystemController.java
@@ -0,0 +1,103 @@
+package cn.evanzuo.admin.business.menu.controller;
+
+import cn.evan.zuo.common.entity.CommonMenuList;
+import cn.evanzuo.admin.business.menu.VO.MenuListVo;
+import cn.evanzuo.admin.business.menu.VO.MenuVo;
+import cn.evanzuo.admin.business.menu.VO.Meta;
+import cn.evanzuo.admin.business.menu.service.imp.IDeptServiceImp;
+import cn.hutool.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+
+@RestController
+@RequestMapping("/system")
+public class SystemController {
+    private final static Logger LOGGER = LoggerFactory.getLogger(SystemController.class);
+    @Autowired
+    IDeptServiceImp iDeptServiceImp;
+
+    @GetMapping("/getDeptList")
+    public MenuVo getDeptList(HttpServletRequest request) {
+        // 从Header中获取用户信息
+        String userStr = request.getHeader("user");
+        JSONObject userJsonObject = new JSONObject(userStr);
+        System.out.println((userJsonObject));
+
+        List<String> authorities = (List<String>)userJsonObject.get("authorities");
+        String authoritiesStr = authorities.stream()
+                .map(item -> "`" + item + "`")
+                .map(item -> item.replace("`", "'"))
+                .collect(Collectors.joining(","));
+        LOGGER.info(authorities.toString());
+        LOGGER.info(authoritiesStr);
+        System.out.println(authorities);
+        List<CommonMenuList> allMenus = iDeptServiceImp.getBaseMapper().getRoleNames(authoritiesStr);
+        MenuVo menuVo = new MenuVo();
+        List<CommonMenuList> projectMenus = allMenus.stream()
+                .filter(item -> item.getParentCid() == 0)
+                .peek(item -> item.setChildren(SystemController.getChildren(item, allMenus)))
+                .sorted(Comparator.comparingInt(CommonMenuList::getSort).reversed())
+                .collect(Collectors.toList());
+
+        menuVo.setList(format(projectMenus));
+        LOGGER.info(projectMenus.toString());
+        LOGGER.info(String.valueOf(projectMenus.size()));
+        menuVo.setTotal(projectMenus.size());
+        return menuVo;
+    }
+    public List<MenuListVo> format(
+            List<CommonMenuList> commonMenuLists
+    ) {
+        if (commonMenuLists.size() == 0) {
+            return new ArrayList<>();
+        }
+        return commonMenuLists.stream().map(
+                item -> {
+                    MenuListVo menuListVo = new MenuListVo();
+                    // meta
+                    Meta meta = new Meta();
+                    meta.setIcon(item.getIcon());
+                    meta.setTitle(item.getTitle());
+                    meta.setHideChildrenInMenu(item.getHideChildrenInMenu());
+                    meta.setHideMenu(item.getHideMenu());
+                    meta.setHideBreadcrumb(item.getHideBreadcrumb());
+                    meta.setCurrentActiveMenu(item.getCurrentActiveMenu());
+                    menuListVo.setMeta(meta);
+                    // 其他字段
+                    menuListVo.setComponent(item.getComponent());
+                    menuListVo.setPath(item.getPath());
+                    menuListVo.setName(item.getName());
+                    menuListVo.setRedirect(item.getRedirect());
+                    menuListVo.setChildren(format(item.getChildren()));
+                    return menuListVo;
+                }
+        ).collect(Collectors.toList());
+    }
+
+    public static List<CommonMenuList> getChildren(CommonMenuList root, List<CommonMenuList> allMenus) {
+        return allMenus.stream()
+                .filter(item -> Objects.equals(item.getParentCid(), root.getCatId()))
+                .peek(item -> item.setChildren(SystemController.getChildren(item, allMenus)))
+                .sorted(Comparator.comparingInt(CommonMenuList::getSort).reversed())
+                .collect(Collectors.toList());
+    }
+    @GetMapping("/getAccountList")
+    public MenuVo getAccountList(HttpServletRequest request) throws UnsupportedEncodingException {
+        MenuVo menuVo = new MenuVo();
+        menuVo.setTotal(2);
+        return  menuVo;
+    }
+}
---
+++ business-core/business-menu/src/main/java/cn/evanzuo/admin/business/menu/service/imp/IDeptServiceImp.java
@@ -0,0 +1,19 @@
+package cn.evanzuo.admin.business.menu.service.imp;
+
+import cn.evan.zuo.common.entity.CommonMenuList;
+import cn.evanzuo.admin.business.menu.mapper.DeptMapper;
+import cn.evanzuo.admin.business.menu.service.IDeptService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author linlaoshi
+ * @since 2022-12-31
+ */
+@Service
+public class IDeptServiceImp extends ServiceImpl<DeptMapper, CommonMenuList> implements IDeptService {
+}
