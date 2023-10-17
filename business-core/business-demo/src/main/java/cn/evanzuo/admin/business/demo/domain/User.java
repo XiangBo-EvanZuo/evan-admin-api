41c6de40969acaaed00464522652b79900884f2e
Evan Zuo <739221432@qq.com>
2023-10-17 17:15:29 +0800

    feat: 逻辑删除

--- business-core/business-demo/src/main/java/cn/evanzuo/admin/business/demo/domain/User.java
+++ business-core/business-demo/src/main/java/cn/evanzuo/admin/business/demo/domain/User.java
@@ -2,20 +2,22 @@ package cn.evanzuo.admin.business.demo.domain;
 
 import java.util.List;
 
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableName;
-import lombok.Builder;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
+import lombok.*;
 
 /**
  * @author EvanZuo[739221432@qq.com] 2023/09/24
  */
 @Data
 @EqualsAndHashCode(callSuper = false)
-@Builder(toBuilder = true)
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
   private Long id;
   private String username;
   private String password;
+  @TableField(exist = false)
   private List<String> roles;
+  private Boolean deleted;
 }
