bbb2915f9178ed5177da54ab6c17e478c4f6963e
Evan Zuo <739221432@qq.com>
2023-11-30 18:30:32 +0800

    feat: sqls

--- sql/sql-ddd-1130/DDL/user.sql
+++ sql/sql-ddd-1130/DDL/user.sql
@@ -81,3 +81,67 @@ create table if not exists tb_wang_user_role_relation
     role_id int        null,
     user_id int        null
 );
+
+create table if not exists tb_wang_menu_category
+(
+    cat_id                bigint auto_increment comment '分类id'
+    primary key,
+    name                  char(50)                     null comment '分类名称',
+    parent_cid            bigint                       null comment '父分类id',
+    cat_level             int                          null comment '层级',
+    show_status           tinyint     default 1        null comment '是否显示[0-不显示，1显示]',
+    sort                  int         default 0        null comment '排序',
+    component             varchar(50) default 'LAYOUT' null,
+    path                  varchar(100)                 null,
+    redirect              varchar(50)                  null,
+    hide_children_in_menu int         default 0        null,
+    icon                  varchar(50)                  null,
+    title                 varchar(50)                  null,
+    hide_menu             tinyint(1)  default 0        null,
+    hide_breadcrumb       tinyint(1)  default 0        null,
+    current_active_menu   varchar(50)                  null
+    )
+    comment '商品三级分类' auto_increment = 1444;
+
+create index parent_cid
+    on tb_wang_menu_category (parent_cid);
+
+
+create table if not exists tb_wang_role_category_relation
+(
+    id          int auto_increment
+    primary key,
+    category_id int null,
+    role_id     int null,
+    constraint id
+    unique (id)
+    )
+    auto_increment = 1;
+
+create table if not exists tb_wang_user_dept
+(
+    cat_id      bigint auto_increment
+    primary key,
+    dept_name   varchar(50)                        null,
+    parent_cid  bigint   default 1                 null,
+    cat_level   int      default 1                 null,
+    sort        int      default 0                 null,
+    create_time datetime default CURRENT_TIMESTAMP null,
+    remark      varchar(100)                       null,
+    status      int      default 0                 null,
+    constraint cat_id
+    unique (cat_id)
+    )
+    auto_increment = 1;
+
+create table if not exists tb_wang_user_dept_relation
+(
+    id      int auto_increment
+    primary key,
+    user_id int null,
+    dept_id int null,
+    constraint id
+    unique (id)
+    )
+    auto_increment = 1;
+
--- sql/sql-ddd-1130/INSERT/user.sql
+++ sql/sql-ddd-1130/INSERT/user.sql
@@ -1,4 +1,4 @@
-insert into MY_TABLE (id, status, module_id, path, deleted)
+insert into tb_wang_auth_url (id, status, module_id, path, deleted)
 values  (1, 1, 1, '/hello', 0),
         (2, 1, 1, '/project/userProjectList', 0),
         (3, 1, 2, '/menu/list', 0),
@@ -29,16 +29,16 @@ values  (1, 1, 1, '/hello', 0),
         (108, 1, 2, '/system/updateRoleMenu', 0),
         (109, 1, 2, '/demo2', 0);
 
-insert into MY_TABLE (id, path, name)
+insert into tb_wang_business_modules (id, path, name)
 values  (1, '/order', '订单'),
         (2, '/user', '用户'),
         (3, '/auth', '鉴权'),
         (4, '/pay', '支付');
-insert into MY_TABLE (id, value, role_name, remarks, role_status)
+insert into tb_wang_role (id, value, role_name, remarks, role_status)
 values  (1, 'ADMIN', '管理员', '1', 1),
         (2, 'USER', '普通用户', '2', 1),
         (3, 'SUPER ADMIN', '超级管理员', '3', 1);
-insert into MY_TABLE (id, role_id, url_id)
+insert into tb_wang_url_role_relation (id, role_id, url_id)
 values  (134, '3', '1'),
         (135, '3', '2'),
         (136, '3', '20'),
@@ -91,17 +91,65 @@ values  (134, '3', '1'),
         (183, '1', '22'),
         (184, '1', '23'),
         (185, '1', '108');
-insert into MY_TABLE (id, username, password, mobile, pwd, deleted, version, avatar_url, enabled, credentials_non_expired, account_non_expired, account_non_locked)
+insert into tb_wang_user (id, username, password, mobile, pwd, deleted, version, avatar_url, enabled, credentials_non_expired, account_non_expired, account_non_locked)
 values  (1, 'admin', '$2a$10$DfEiVYQiJk3zL/tpqIN5heat8BqU6X1wNCZj1aqEsf9GRPYKoMyQC', '1', '$2a$10$DfEiVYQiJk3zL/tpqIN5heat8BqU6X1wNCZj1aqEsf9GRPYKoMyQC', 0, null, null, 1, 1, 1, 1),
         (2, 'user', '$2a$10$DfEiVYQiJk3zL/tpqIN5heat8BqU6X1wNCZj1aqEsf9GRPYKoMyQC', null, '$2a$10$DfEiVYQiJk3zL/tpqIN5heat8BqU6X1wNCZj1aqEsf9GRPYKoMyQC', 0, null, null, 1, 1, 1, 1);
 
-insert into MY_TABLE (user_id, create_time, email, nick_name, remark)
+insert into tb_wang_user_extra (user_id, create_time, email, nick_name, remark)
 values  (1, '2023-10-08 15:30:45', '739221432@qq.com', 'admin-bo', '1'),
         (2, '2023-10-08 15:30:58', '1157778986@qq.com', 'user-wang', '2');
 
-insert into MY_TABLE (id, role_id, user_id)
+insert into tb_wang_user_role_relation (id, role_id, user_id)
 values  ('1', 1, 1),
         ('2', 2, 2),
         ('3', 2, 1),
         ('4', 3, 1);
 
+insert into tb_wang_menu_category (cat_id, name, parent_cid, cat_level, show_status, sort, component, path, redirect, hide_children_in_menu, icon, title, hide_menu, hide_breadcrumb, current_active_menu)
+values  (1, 'Dashboard', 0, 1, 1, 0, 'LAYOUT', '/dashboard', '/dashboard/analysis', 1, 'bx:bx-home', 'routes.dashboard.dashboard', 0, 0, '/dashboard'),
+        (2, 'Analysis', 1, 2, 1, 0, '/dashboard/analysis/index', 'analysis', '', 0, 'bx:bx-home', 'routes.dashboard.dashboard', 1, 1, null),
+        (3, 'Workbench', 1, 2, 1, 0, '/dashboard/workbench/index', 'workbench', '', 0, 'bx:bx-home', 'routes.dashboard.dashboard', 1, 1, null),
+        (4, 'Permission', 0, 1, 1, 0, 'LAYOUT', '/permission', '/permission/back/btn', 0, 'carbon:user-role', 'routes.demo.permission.permission', 0, 0, null),
+        (1433, 'PermissionBackDemo', 4, 2, 1, 0, '', 'back', '', 0, null, 'routes.demo.permission.back', 0, 0, null),
+        (1434, 'BackAuthBtn', 1433, 3, 1, 0, '/demo/permission/back/Btn', 'btn', null, 0, null, 'routes.demo.permission.backBtn', 0, 0, null),
+        (1435, 'BackAuthPage', 1433, 3, 1, 0, '/demo/permission/back/index', 'page', null, 0, null, 'routes.demo.permission.backPage', 0, 0, null),
+        (1436, 'System', 0, 1, 1, 0, 'LAYOUT', '/system', '/system/account', 0, 'ion:settings-outline', 'routes.demo.system.moduleName', 0, 0, null),
+        (1437, 'AccountManagement', 1436, 2, 1, 0, '/demo/system/account/index', 'account', null, 0, 'ion:settings-outline', 'routes.demo.system.account', 0, 0, null),
+        (1438, 'AccountDetail', 1436, 2, 1, 0, '/demo/system/account/AccountDetail', 'account_detail/:id', null, 0, 'ion:settings-outline', 'routes.demo.system.account_detail', 1, 0, '/system/account'),
+        (1439, 'RoleManagement', 1436, 2, 1, 0, '/demo/system/role/index', 'role', null, 0, 'ion:settings-outline', 'routes.demo.system.role', 0, 0, null),
+        (1440, 'MenuManagement', 1436, 2, 1, 0, '/demo/system/menu/index', 'menu', null, 0, 'ion:settings-outline', 'routes.demo.system.menu', 0, 0, null),
+        (1441, 'DeptManagement', 1436, 2, 1, 0, '/demo/system/dept/index', 'dept', null, 0, 'ion:settings-outline', 'routes.demo.system.dept', 0, 0, null),
+        (1442, 'ChangePassword', 1436, 2, 1, 0, '/demo/system/password/index', 'changePassword', null, 0, 'ion:settings-outline', 'routes.demo.system.password', 0, 0, null),
+        (1443, 'URL资源管理', 1436, 2, 1, 0, '/demo/system/url/index', 'urlResource', null, 0, 'ion:settings-outline', 'routes.demo.system.url', 0, 0, null);
+
+insert into tb_wang_role_category_relation (id, category_id, role_id)
+values  (6, 1, 3),
+        (7, 4, 3),
+        (8, 1436, 3),
+        (12, 1, 1),
+        (13, 4, 1),
+        (14, 1436, 1),
+        (18, 1, 2),
+        (19, 4, 2);
+
+insert into tb_wang_user_dept (cat_id, dept_name, parent_cid, cat_level, sort, create_time, remark, status)
+values  (1, 'Dashboard', 0, 1, 0, '2023-10-05 11:04:51', null, 0),
+        (2, 'Analysis', 1, 2, 0, '2023-10-05 11:04:51', null, 0),
+        (3, 'Workbench', 1, 2, 0, '2023-10-05 11:04:51', null, 0),
+        (4, 'Permission', 0, 1, 0, '2023-10-05 11:04:51', null, 0),
+        (1433, 'PermissionBackDemo', 4, 2, 0, '2023-10-05 11:04:51', null, 0),
+        (1434, 'BackAuthBtn', 1433, 3, 0, '2023-10-05 11:04:51', null, 0),
+        (1435, 'BackAuthPage', 1433, 3, 0, '2023-10-05 11:04:51', null, 0),
+        (1436, 'System', 0, 1, 0, '2023-10-05 11:04:51', null, 0),
+        (1437, 'AccountManagement', 1436, 2, 0, '2023-10-05 11:04:51', null, 0),
+        (1438, 'AccountDetail', 1436, 2, 0, '2023-10-05 11:04:51', null, 0),
+        (1439, 'RoleManagement', 1436, 2, 0, '2023-10-05 11:04:51', null, 0),
+        (1440, 'MenuManagement', 1436, 2, 0, '2023-10-05 11:04:51', null, 0),
+        (1441, 'DeptManagement', 1436, 2, 0, '2023-10-05 11:04:51', null, 0),
+        (1442, 'ChangePassword', 1436, 2, 0, '2023-10-05 11:04:51', null, 0);
+
+insert into tb_wang_user_dept_relation (id, user_id, dept_id)
+values  (1, 1, 1),
+        (2, 2, 1),
+        (3, 1, 2),
+        (4, 2, 2);
