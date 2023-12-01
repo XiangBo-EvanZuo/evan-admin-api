cfdedab0f5f848b377855a159ea7050fe019b653
Evan Zuo <739221432@qq.com>
2023-12-01 18:38:52 +0800

    add: ddd struct

---
+++ evan-admin-business-core/evan-admin-user/evan-admin-user-infrastructure/src/main/java/cn/evanzuo/admin/business/user/sdk/feign/api/impl/UserFeignProviderImpl.java
@@ -0,0 +1,5 @@
+package cn.evanzuo.admin.business.user.sdk.feign.api.impl;
+import cn.evanzuo.admin.business.user.sdk.feign.api.UserFeignProvider;
+
+public class UserFeignProviderImpl implements UserFeignProvider {
+}
