f5b290339e969b0a1a66eab12234a5e4cb22bb7e
Evan Zuo <739221432@qq.com>
2023-12-05 15:35:35 +0800

    feat: org.mapstruct dependencyManagement

--- evan-admin-business-core/evan-admin-user/evan-admin-user-infrastructure/src/main/java/cn/evan/admin/user/infrastructure/repository/menu/UserMenuPOConvertor.java
+++ evan-admin-business-core/evan-admin-user/evan-admin-user-infrastructure/src/main/java/cn/evan/admin/user/infrastructure/repository/menu/UserMenuPOConvertor.java
@@ -1,4 +1,11 @@
 package cn.evan.admin.user.infrastructure.repository.menu;
 
-interface UserMenuPOConvertor {
+import cn.evan.admin.user.domain.aggregate.menu.entity.UserMenuEntity;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface UserMenuPOConvertor {
+    List<UserMenuEntity> convertor(List<UserMenuPO> list);
 }
