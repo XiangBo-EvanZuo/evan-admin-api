ee862a83faa7c1af1fa82b81e5af0d627fb8ee8f
Evan Zuo <739221432@qq.com>
2023-12-06 11:02:45 +0800

    order: app init

---
+++ evan-admin-business-core/evan-admin-order/evan-admin-order-application/src/main/java/cn/evan/admin/order/application/convertor/menu/MenuVO2DTO.java
@@ -0,0 +1,13 @@
+package cn.evan.admin.order.application.convertor.menu;
+
+import cn.evan.zuo.common.entity.CommonMenuList;
+import org.mapstruct.Mapper;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Mapper(componentModel = "spring")
+public interface MenuVO2DTO {
+
+}
---
+++ evan-admin-business-core/evan-admin-order/evan-admin-order-application/src/main/java/cn/evan/admin/order/application/service/menu/MenuApplication.java
@@ -0,0 +1,12 @@
+package cn.evan.admin.order.application.service.menu;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class MenuApplication {
+
+}
+
+
