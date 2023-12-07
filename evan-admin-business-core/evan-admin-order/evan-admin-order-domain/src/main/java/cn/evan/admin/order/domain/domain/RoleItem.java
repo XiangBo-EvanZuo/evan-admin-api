73dac1c8a83ecc97d1fe07556ff9e1050d1e5c42
Evan Zuo <739221432@qq.com>
2023-12-07 15:21:00 +0800

    del: useless code

--- evan-admin-business-core/evan-admin-order/evan-admin-order-domain/src/main/java/cn/evan/admin/order/domain/domain/RoleItem.java
+++
@@ -1,9 +0,0 @@
-package cn.evan.admin.order.domain.domain;
-
-import lombok.Data;
-
-@Data
-public class RoleItem {
-    String roleName;
-    String value;
-}
--- evan-admin-business-core/evan-admin-order/evan-admin-order-domain/src/main/java/cn/evan/admin/order/domain/service/IUserService.java
+++
@@ -1,7 +0,0 @@
-package cn.evan.admin.order.domain.service;
-
-import cn.evan.admin.order.domain.domain.User;
-import com.baomidou.mybatisplus.extension.service.IService;
-
-public interface IUserService extends IService<User> {
-}
