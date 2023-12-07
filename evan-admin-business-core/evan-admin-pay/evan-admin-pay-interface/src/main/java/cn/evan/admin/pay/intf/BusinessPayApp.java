27800032aa94aefdf6c604345aa4a926bd36502e
Evan Zuo <739221432@qq.com>
2023-12-07 14:52:45 +0800

    rename

--- evan-admin-business-core/evan-admin-pay/evan-admin-pay-interface/src/main/java/cn/evan/admin/pay/intf/BusinessPayApp.java
+++ evan-admin-business-core/evan-admin-pay/evan-admin-pay-interface/src/main/java/cn/evan/admin/pay/intf/BusinessPayApp.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author EvanZuo[739221432@qq.com] 2023/09/24
@@ -12,7 +13,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableFeignClients(basePackages = "cn.evan.admin.common.feign.client.clients")
 @EnableDiscoveryClient
 @SpringBootApplication
-@MapperScan("cn.evan.admin.pay.domain.mapper")
+@MapperScan("cn.evan.admin.pay.intf.mapper")
 public class BusinessPayApp {
 
   public static void main(String[] args) {
--- evan-admin-business-core/evan-admin-pay/evan-admin-pay-interface/src/main/java/cn/evan/admin/pay/intf/controller/HelloController.java
+++ evan-admin-business-core/evan-admin-pay/evan-admin-pay-interface/src/main/java/cn/evan/admin/pay/intf/controller/HelloController.java
@@ -1,41 +1,31 @@
 package cn.evan.admin.pay.intf.controller;
 
-import cn.evan.admin.pay.intf.domain.User;
-import cn.evan.admin.pay.intf.service.imp.UserServiceDBImpl;
 import cn.evan.admin.common.feign.client.clients.EvanFeignUserInfo;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
-import java.util.List;
 
 /**
  * @author EvanZuo[739221432@qq.com] 2023/09/24
  */
+@Slf4j
 @RestController
 public class HelloController {
-  private final static Logger LOGGER = LoggerFactory.getLogger(HelloController.class);
   @Autowired
   EvanFeignUserInfo evanFeignUserInfo;
 
-  @Resource
-  private UserServiceDBImpl userServiceDB;
   @GetMapping("/feign")
   public String feign(HttpServletRequest request) {
 
     // 从Header中获取用户信息
     String userStr = request.getHeader("user");
     String res = evanFeignUserInfo.getUserMenuList(userStr);
-    LOGGER.info("/feign res:{}", res);
+    log.info("/feign res:{}", res);
     return "Hello World ! feign";
-  }
-  @GetMapping("/userList")
-  public List<User> userList() {
-    return userServiceDB.list();
   }
 }
 
