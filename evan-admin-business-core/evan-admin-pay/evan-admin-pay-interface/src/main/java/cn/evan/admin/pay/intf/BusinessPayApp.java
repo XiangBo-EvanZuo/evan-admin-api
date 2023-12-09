4cef53909349c35bf0fe8d215311a76c9365719b
Evan Zuo <739221432@qq.com>
2023-12-10 03:31:00 +0800

    feat: rabbit mq init

--- evan-admin-business-core/evan-admin-pay/evan-admin-pay-interface/src/main/java/cn/evan/admin/pay/intf/BusinessPayApp.java
+++ evan-admin-business-core/evan-admin-pay/evan-admin-pay-interface/src/main/java/cn/evan/admin/pay/intf/BusinessPayApp.java
@@ -1,10 +1,13 @@
 package cn.evan.admin.pay.intf;
 
+import cn.evan.admin.pay.intf.mq.messaging.OrderSink;
+import cn.evan.admin.pay.intf.mq.messaging.OrderSource;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
@@ -13,6 +16,7 @@ import org.springframework.context.annotation.ComponentScan;
 @EnableFeignClients(basePackages = "cn.evan.admin.common.feign.client.clients")
 @EnableDiscoveryClient
 @SpringBootApplication
+@EnableBinding({OrderSource.class, OrderSink.class})
 @MapperScan("cn.evan.admin.pay.intf.mapper")
 public class BusinessPayApp {
 
