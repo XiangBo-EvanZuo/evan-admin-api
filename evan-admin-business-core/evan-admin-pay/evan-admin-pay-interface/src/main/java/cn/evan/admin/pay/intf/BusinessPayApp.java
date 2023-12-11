961506b0405fc1f31bfb4eca325f08a8aefbecd8
Evan Zuo <739221432@qq.com>
2023-12-11 15:55:35 +0800

    feat: rename door

--- evan-admin-business-core/evan-admin-pay/evan-admin-pay-interface/src/main/java/cn/evan/admin/pay/intf/BusinessPayApp.java
+++ evan-admin-business-core/evan-admin-pay/evan-admin-pay-interface/src/main/java/cn/evan/admin/pay/intf/BusinessPayApp.java
@@ -1,30 +1,30 @@
 package cn.evan.admin.pay.intf;
+
+import cn.evan.admin.pay.domain.mq.messaging.OrderSink;
+import cn.evan.admin.pay.domain.mq.messaging.OrderSource;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.context.annotation.ComponentScan;
+
 /**
  * @author EvanZuo[739221432@qq.com] 2023/09/24
  */
 @EnableFeignClients(basePackages = "cn.evan.admin.common.feign.client.clients")
 @EnableDiscoveryClient
 @SpringBootApplication
-@MapperScan({
-        "cn.evan.admin.user.domain.mapper",
-        "cn.evan.admin.user.infrastructure.repository"
-})
+@EnableBinding({OrderSource.class, OrderSink.class})
+@MapperScan("cn.evan.admin.pay.domain.mapper")
 @ComponentScan(basePackages = {
-        "cn.evan.admin.user.domain", // 菜单service
-        "cn.evan.admin.user.application", // 菜单service
-        "cn.evan.admin.user.infrastructure", // 基础service
-        "cn.evan.admin.user.intf", // 入口controller
-        "cn.evan.admin.user.sdk",
+        "cn.evan.admin.pay"
 })
-public class BusinessUserApp {
+public class BusinessPayApp {
 
   public static void main(String[] args) {
-    SpringApplication.run(BusinessUserApp.class, args);
+    SpringApplication.run(BusinessPayApp.class, args);
   }
+
 }
